916479854ff3eca79449045c4f6b45567eabdad4
taeksoo <jazzsaxmafia@gmail.com>
2015-08-06 22:03:21 +0900

    little changes

--- cnn_util.py
+++ cnn_util.py
@@ -4,7 +4,7 @@ import numpy as np
 import skimage
 import ipdb
 
-def makeCropImage(x, target_height=227, target_width=227):
+def crop_image(x, target_height=227, target_width=227):
     print x
     image = skimage.img_as_float(skimage.io.imread(x)).astype(np.float32)
 
@@ -28,19 +28,22 @@ deploy = '/home/taeksoo/Package/caffe/models/bvlc_reference_caffenet/deploy.prot
 model = '/home/taeksoo/Package/caffe/models/bvlc_reference_caffenet/bvlc_reference_caffenet.caffemodel'
 mean = '/home/taeksoo/Package/caffe/python/caffe/imagenet/ilsvrc_2012_mean.npy'
 
-class CNNUtil(object):
+class CNN(object):
 
-    def __init__(self, deploy=deploy, model=model, mean=mean, batch_size=10, width=227, height=227):
+    def __init__(self, deploy=deploy, model=model, mean=mean, batch_size=100, width=227, height=227):
 
         self.deploy = deploy
         self.model = model
         self.mean = mean
 
         self.batch_size = batch_size
-        self.net, self.transformer = self.getNet()
+        self.net, self.transformer = self.get_net()
         self.net.blobs['data'].reshape(self.batch_size, 3, height, width)
 
-    def getNet(self):
+        self.width = width
+        self.height = height
+
+    def get_net(self):
         caffe.set_mode_gpu()
         net = caffe.Net(self.deploy, self.model, caffe.TEST)
 
@@ -52,15 +55,15 @@ class CNNUtil(object):
 
         return net, transformer
 
-    def imageFeatures(self, image_list, layers='fc7', layer_sizes=4096):
+    def get_features(self, image_list, layers='fc7', layer_sizes=[4096]):
         iter_until = len(image_list) + self.batch_size
-        all_feats = np.zeros((len(image_list), layer_sizes))
+        all_feats = np.zeros([len(image_list)] + layer_sizes)
 
         for start, end in zip(range(0, iter_until, self.batch_size), \
                               range(self.batch_size, iter_until, self.batch_size)):
 
             image_batch_file = image_list[start:end]
-            image_batch = np.array(map(lambda x: makeCropImage(x, target_width=227, target_height=227), image_batch_file))
+            image_batch = np.array(map(lambda x: crop_image(x, target_width=self.width, target_height=self.height), image_batch_file))
 
             caffe_in = np.zeros(np.array(image_batch.shape)[[0,3,1,2]], dtype=np.float32)
 
@@ -73,7 +76,6 @@ class CNNUtil(object):
 
             all_feats[start:end] = feats
 
-        ipdb.set_trace()
         return all_feats
 
 
